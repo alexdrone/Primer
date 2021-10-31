93adb2d2a569aa425dbaa0be973e62685f19de0e
Alex Usbergo <alexusbergo@google.com>
2021-10-31 11:12:59 +0100

    Public init for PropertyObservable.

--- Sources/Primer/PropertyObservable.swift
+++ Sources/Primer/PropertyObservable.swift
@@ -17,6 +17,12 @@ public struct AnyPropertyChangeEvent {
   /// Optional debug label for this event.
   public let debugLabel: String?
   
+  public init(object: Any, keyPath: AnyKeyPath? = nil, debugLabel: String? = nil) {
+    self.object = object
+    self.keyPath = keyPath
+    self.debugLabel = debugLabel
+  }
+  
   /// Returns the tuple `object, value` if this property change matches the `keyPath` passed as
   /// argument.
   public func match<T, V>(keyPath: KeyPath<T, V>) -> (T, V)? {
